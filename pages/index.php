f2221dd828e7c67c0d5da9de02f8694497976bc3
manel-amrous <manel.amrous@laplateforme.io>
2025-02-19 10:27:09 +0100

    front page daccueil

--- pages/index.php
+++ pages/index.php
@@ -9,6 +9,8 @@
        <h1>Bienvenue sur notre livre d'or</h1>
     </div>
 
+    <div class="colonne">
     <a href="inscription.php">S'inscrire</a> | <a href="connexion.php">Se connecter</a> | <a href="livre-or.php">Voir le livre d'or</a>
+    </div>
 </body>
 </html>
